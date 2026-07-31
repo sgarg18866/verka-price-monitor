eeeefa7b8ce18e38b9dd78daa5dea0f9a2907edd
sgarg18866 <sgarg1886@gmail.com>
2026-07-31 21:09:12 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 
 # ===== CONFIG =====
 PRICE_THRESHOLD = 27
@@ -48,12 +47,12 @@ def save_price(price):
 def fetch_price():
     r = requests.post(URL, headers=HEADERS, json=BODY, timeout=20)
 
-   print("Status Code:", r.status_code)
-   print("Content-Type:", r.headers.get("content-type"))
-   print("Response:")
-   print(r.text[:1000])
+    print("Status Code:", r.status_code)
+    print("Content-Type:", r.headers.get("content-type"))
+    print("Response:")
+    print(r.text[:1000])
 
-   return None
+    return None
 
 def main():
     price = fetch_price()
